ff86c93a7666ce4b016c50fad4f83b56f2ba543f
Kiara <33493552+Kbarias@users.noreply.github.com>
2018-10-31 12:43:33 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "MazeSolver.h"
+
+int main()
+{
+	MazeSolver solver("inputB.txt");
+	if(solver.mazeIsReady())
+	{
+		solver.solveMaze();
+		solver.printSolution();
+ 	}
+ 	return 0;
+}
